508d347329323ddfae64fae314e2ee1125374e2b
stepin104445 <69110250+stepin104445@users.noreply.github.com>
2020-09-17 12:27:05 +0530

    Update sum.h

--- inc/sum.h
+++ inc/sum.h
@@ -1,16 +1,6 @@
-/**
-* @file factorial.h
-*
-*/
 #ifndef __SUM_H__
 #define __SUM_H__
 
-/**
-* Calculates the factorial of integer number
-* @param[in] number for which factorial has to be found
-* @return Factorial of the number
-* @note Returns -1 for negative values
-*/
-int sum(int number);
+int sum(int number1, int number2);
 
-#endif /* #ifndef __SUM_H__ */
+#endif
