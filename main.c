70d6662dbc5243ab8ca4024787649c05750877e0
stepin104445 <69110250+stepin104445@users.noreply.github.com>
2020-09-18 16:47:12 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include<math.h>
 	#include "sum.h"					
 	
 	int main()
 	{
     	printf("Hello world!\n");
 
-		int sum_op = sum(20, 20);				
+		int sum_op = sum(20, 20);
+		int k=sqrt(9);
 		printf("Sum = %d", sum_op);
 
 
